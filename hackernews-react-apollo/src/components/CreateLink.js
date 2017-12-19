80dc7548474bdd7e8050d598770a6228a584fc44
Mikolaj Dziurzynski <dziurzynskim@gmail.com>
2017-12-19 17:09:47 +0100

    Graphql mutations - ERROR

---
+++ hackernews-react-apollo/src/components/CreateLink.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react"
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+
+class CreateLink extends Comment {
+    
+    state = {
+        description: '',
+        url: ''
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="flex flex-columnt mt3">
+                    <input
+                        className='mb2'
+                        value={this.state.description}
+                        onChange={(e) => this.setState({ description: e.target.value })}
+                        type='text'
+                        placeholder='A description for the link'
+                    />
+                    <input
+                        className='mb2'
+                        value={this.state.url}
+                        onChange={(e) => this.setState({ url: e.target.value })}
+                        type='text'
+                        placeholder="The URL for the link"
+                    />
+                </div>
+                <button
+                    onClick={() => this._createLink()}
+                >
+                Submit
+                </button>
+            </div>
+        )
+
+    }
+
+    _createLink = async () => {
+        const { description, url } = this.state
+        await this.props.createLinkMutation({
+           variables: {
+            description,
+            url
+           }
+        })
+    }
+
+}
+
+// 1
+const CREATE_LINK_MUTATION = gql`
+    # 2
+    mutation CreateLinkMutation($description: String!, $url: String!) {
+        createLink(
+            description: $description,
+            url: $url,
+        ) {
+            id
+            createdAt
+            url
+            description
+        }
+    }
+`
+
+// 3
+export default graphql(CREATE_LINK_MUTATION, { name: 'createLinkMutation' })(CreateLink)
